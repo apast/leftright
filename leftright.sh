40b2f7c6584fda698547f000791c9ad58931dd31
Andre Pastore <pastore@mondli.com>
2017-05-21 22:29:57 -0300

    fix bash script

--- leftright.sh
+++ leftright.sh
@@ -5,15 +5,15 @@ set -e
 
 ROOT_DIR="$(dirname $0)/"
 
-VENV_ROOT="$ROOT_DIR}venv/"
+VENV_ROOT="${ROOT_DIR}venv/"
+
 virtualenv "$VENV_ROOT"
-. ${VENV_ROOT}bin/activate
 
 cd $ROOT_DIR
 
 pip install -r requirements.txt
 
-cd ${ROOT_DIR}src
+cd "${ROOT_DIR}src"
 python -m leftright -p 8080
 
 set +e
